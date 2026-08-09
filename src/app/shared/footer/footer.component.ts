18e16f6a449e65b2f6bd64ec62e954261b913ea7
Thanik Cheowtirakul <thanikc@gmail.com>
2026-08-09 12:43:16 +0200

    feat(footer): add footer component with linkedin link

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {}
