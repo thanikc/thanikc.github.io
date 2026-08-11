99b33a3dd592f6ba4997eb790be2dc88117c8334
Thanik Cheowtirakul <thanikc@gmail.com>
2026-08-11 21:35:03 +0200

    feat(SSG): add angular SSR for Static Site Generation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideAnalytics } from './shared/analytics/analytics.provider';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
     provideAnalytics(),
+    provideClientHydration(withEventReplay()),
   ],
 };
