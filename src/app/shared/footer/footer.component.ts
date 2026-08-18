1ec6af78290bb82f9ea1e1aaa8544c79faaa5c8e
Thanik Cheowtirakul <thanikc@gmail.com>
2026-08-18 21:34:31 +0200

    feat(ui): optimize navigation

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,7 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { AdBannerToggleComponent } from '../../domains/ads/ad-banner-toggle.component';
 
 @Component({
   selector: 'app-footer',
+  imports: [AdBannerToggleComponent],
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
